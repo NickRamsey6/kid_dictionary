a5eaadab8e5848bdc37a550031699da74f0af75a
Nick Ramsey <nramseysc@gmail.com>
2019-07-26 10:43:31 -0700

    add methods for Word class that allow CRUD functionality and passing unit tests

--- lib/word.rb
+++ lib/word.rb
@@ -1,3 +1,37 @@
 class Word
+  attr_reader :id
+  attr_accessor :term
 
+  @@words = {}
+  @@total_rows = 0
+
+  def initialize(attributes)
+    @term = attributes.fetch(:term)
+    @id = id || @@total_rows +=1
+  end
+  # def iniitalize(term, id)
+  #   @term = term
+  #   @id = id || @@total_rows +=1
+  # end
+
+  def self.all
+    @@words.values()
+  end
+
+  def self.clear
+    @@words = {}
+    @@total_rows = 0
+  end
+
+  def create
+    @@words[self.id] = self
+  end
+
+  def delete
+    @@words.delete(self.id)
+  end
+
+  def edit(term)
+    self.term = term
+  end
 end
--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -1,7 +1,40 @@
-require 'rspec'
-require 'pry'
-require 'word'
+require ('rspec')
+require ('pry')
+require ('word')
 
 describe('#Word') do
+  before(:each) do
+    Word.clear()
+  end
+
+  describe('.create') do
+    it('creates a new word') do
+      test_word = Word.new(:term =>'word')
+      test_word.create
+      @list_of_words = Word.all
+      expect(@list_of_words).to(eq([test_word]))
+    end
+  end
+
+  describe('.delete') do
+    it('deletes a created word') do
+      test_word = Word.new(:term =>'word')
+      test_word.create
+      test_word.delete
+      @list_of_words = Word.all
+      expect(@list_of_words).to(eq([]))
+    end
+  end
+
+  describe('.edit') do
+    it('edits a created word') do
+      test_word = Word.new(:term =>'word')
+      test_word.create
+      test_word.edit('noun')
+      expect(test_word.term).to(eq('noun'))
+    end
+  end
+
+
 
 end
