f5c6838d930fa5c9412327aacb2fd019ce0a43a8
Nick Ramsey <nramseysc@gmail.com>
2019-07-26 11:21:19 -0700

    add methods for Define module

--- lib/word.rb
+++ lib/word.rb
@@ -1,37 +1,87 @@
-class Word
-  attr_reader :id
-  attr_accessor :term
+module Term
+  class Word
+    attr_reader :id
+    attr_accessor :term
 
-  @@words = {}
-  @@total_rows = 0
-
-  def initialize(attributes)
-    @term = attributes.fetch(:term)
-    @id = id || @@total_rows +=1
-  end
-  # def iniitalize(term, id)
-  #   @term = term
-  #   @id = id || @@total_rows +=1
-  # end
-
-  def self.all
-    @@words.values()
-  end
-
-  def self.clear
     @@words = {}
     @@total_rows = 0
-  end
 
-  def create
-    @@words[self.id] = self
-  end
+    def initialize(attributes)
+      @term = attributes.fetch(:term)
+      @id = id || @@total_rows +=1
+    end
 
-  def delete
-    @@words.delete(self.id)
-  end
 
-  def edit(term)
-    self.term = term
+    def self.all
+      @@words.values()
+    end
+
+    def self.clear
+      @@words = {}
+      @@total_rows = 0
+    end
+
+    def create
+      @@words[self.id] = self
+    end
+
+    def delete
+      @@words.delete(self.id)
+    end
+
+    def edit(term)
+      self.term = term
+    end
+  end
+end
+
+module Define
+  class Word
+    attr_reader :id
+    attr_accessor :definition, :term_id
+
+    @@definitions = {}
+    @@total_rows = 0
+
+    def initialize(attributes)
+      @definition = attributes.fetch(:definition)
+      @term_id = attributes.fetch(:term_id)
+      @id = id || @@total_rows +=1
+    end
+
+    def self.all
+      @@definitions.values()
+    end
+
+    def self.clear
+      @@definitions = {}
+      @@total_rows = 0
+    end
+
+    def create
+      @@definitions[self.id] = self
+    end
+
+    def delete
+      @@definitions.delete(self.id)
+    end
+
+    def edit(term)
+      @@definitions.definition = definition
+    end
+
+    def term
+      term.find(self.term_id)
+    end
+
+    def self.find_by_term(id)
+      definitions = []
+      @@definitions.values.each do |definition|
+        if definition.term_id == id
+          definitions.push(definition)
+        end
+      end
+      definitions
+    end
   end
 end
--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -2,39 +2,71 @@ require ('rspec')
 require ('pry')
 require ('word')
 
-describe('#Word') do
+describe('#Term') do
   before(:each) do
-    Word.clear()
+    Term::Word.clear()
   end
 
   describe('.create') do
     it('creates a new word') do
-      test_word = Word.new(:term =>'word')
+      test_word = Term::Word.new(:term =>'word')
       test_word.create
-      @list_of_words = Word.all
+      @list_of_words = Term::Word.all
       expect(@list_of_words).to(eq([test_word]))
     end
   end
 
   describe('.delete') do
     it('deletes a created word') do
-      test_word = Word.new(:term =>'word')
+      test_word = Term::Word.new(:term =>'word')
       test_word.create
       test_word.delete
-      @list_of_words = Word.all
+      @list_of_words = Term::Word.all
       expect(@list_of_words).to(eq([]))
     end
   end
 
   describe('.edit') do
     it('edits a created word') do
-      test_word = Word.new(:term =>'word')
+      test_word = Term::Word.new(:term =>'word')
       test_word.create
       test_word.edit('noun')
       expect(test_word.term).to(eq('noun'))
     end
   end
 
+end
 
+describe('#Define') do
+  before(:each) do
+    Define::Word.clear()
+  end
 
+  describe('.create') do
+    it('creates a new definition') do
+      test_def = Define::Word.new(:definition =>'as much food as one man can hold', :term_id => "1")
+      test_def.create
+      @definitions = Define::Word.all
+      expect(@definitions).to(eq([test_def]))
+    end
+  end
+
+  describe('.delete') do
+    it('deletes a created word') do
+      test_def = Define::Word.new(:definition =>'as much food as one man can hold', :term_id => "1")
+      test_def.create
+      test_def.delete
+      @definitions = Define::Word.all
+      expect(@definitions).to(eq([]))
+    end
+  end
+
+  describe('.edit') do
+    it('edits a created word') do
+      test_def = Define::Word.new(:definition =>'as much food as one man can hold', :term_id => "1")
+      test_def.create
+      test_def.edit('the meal between breakfast and dinner')
+      expect(test_def.definition).to(eq('the meal between breakfast and dinner'))
+    end
+  end
 end
